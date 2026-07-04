ca02af4a7603901ec94f6c5d59a2a219f95aa5cb
SerkanPN <123215352+SerkanPN@users.noreply.github.com>
2026-07-05 04:15:08 +0700

    Update connect.ts

--- api/etsy/connect.ts
+++ api/etsy/connect.ts
@@ -24,8 +24,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const scopes = "listings_w listings_r listings_d shops_r shops_w transactions_r transactions_w profile_r email_r";
   const encodedScopes = encodeURIComponent(scopes);
   
-  // Note: For local testing, REDIRECT_URI should be http://localhost:5173/etsy/callback
-  // We will assume process.env.REDIRECT_URI handles this based on environment
   const authUrl = `https://www.etsy.com/oauth/connect?response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodedScopes}&client_id=${ETSY_API_KEY}&state=${state}&code_challenge=${codeChallenge}&code_challenge_method=S256`;
 
   return res.json({ auth_url: authUrl });
